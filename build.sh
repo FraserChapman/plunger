9e5053b647c31566e8abe96db18683037ac089db
Fraser Chapman <fraser.chapman@gmail.com>
2023-11-19 23:11:22 +0000

    Updated build.sh to include LICENCE in archives

--- build.sh
+++ build.sh
@@ -31,15 +31,19 @@ cp -r "$SRC_DIR/"* "$TMP_ZIP_DIR"
 cp -r "$SRC_DIR/"* "$TMP_XPI_DIR"
 echo "Copied $SRC_DIR to [$TMP_ZIP_DIR, $TMP_XPI_DIR]"
 
+cp -r "./LICENSE"* "$TMP_ZIP_DIR"
+cp -r "./LICENSE"* "$TMP_XPI_DIR"
+echo "Copied LICENSE to [$TMP_ZIP_DIR, $TMP_XPI_DIR]"
+
 if ! jq --indent 4 '. + {browser_specific_settings: {gecko: {id: "plunger@fraser"}}}' "$TMP_XPI_DIR/manifest.json" > "$TMP_XPI_DIR/manifest.json.tmp"; then
     echo "Failed to add browser-specific settings to Firefox manifest"
     exit 1
 fi
-echo "Added browser-specific settings to $TMP_XPI_DIR/manifest.json {browser_specific_settings: {gecko: {id: \"plunger@fraser\"}}}"
+echo "Added browser-specific settings to Firefox manifest"
 
 mv "$TMP_XPI_DIR/manifest.json.tmp" "$TMP_XPI_DIR/manifest.json"
 
-( cd "$TMP_ZIP_DIR" && zip -r "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.zip" * )
-( cd "$TMP_XPI_DIR" && zip -r "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.xpi" * )
+( cd "$TMP_ZIP_DIR" && zip -T -u "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.zip" * )
+( cd "$TMP_XPI_DIR" && zip -T -u "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.xpi" * )
 
-echo "Build $VERSION complete: $BUILD_DIR [plunger@$VERSION.xpi, plunger@$VERSION.zip]"
+echo "Build $VERSION complete:  [$BUILD_DIR/plunger@$VERSION.xpi, $BUILD_DIR/plunger@$VERSION.zip]"
