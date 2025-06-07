364b640ca3e7df4c31b78d1ea320f88c5bb76013
Fraser Chapman <fraser.chapman@gmail.com>
2025-06-08 00:11:42 +0100

    update build script to account for _locales and sub directories

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ fi
 echo "Build directory confirmed: $BUILD_DIR"
 
 if [ "$#" -eq 1 ]; then
-    if ! jq --indent 4 "(.version |= \"$VERSION\")" "$SRC_DIR/manifest.json" > "$SRC_DIR/manifest.json.tmp"; then
+    if ! jq --indent 4 "(.version |= \"$VERSION\")" "$SRC_DIR/manifest.json" >"$SRC_DIR/manifest.json.tmp"; then
         echo "Failed to update version in manifest.json"
         exit 1
     fi
@@ -35,7 +35,7 @@ cp -r "./LICENSE"* "$TMP_ZIP_DIR"
 cp -r "./LICENSE"* "$TMP_XPI_DIR"
 echo "Copied LICENSE to [$TMP_ZIP_DIR, $TMP_XPI_DIR]"
 
-if ! jq --indent 4 '. + {browser_specific_settings: {gecko: {id: "plunger@fraser"}}}' "$TMP_XPI_DIR/manifest.json" > "$TMP_XPI_DIR/manifest.json.tmp"; then
+if ! jq --indent 4 '. + {browser_specific_settings: {gecko: {id: "plunger@fraser"}}}' "$TMP_XPI_DIR/manifest.json" >"$TMP_XPI_DIR/manifest.json.tmp"; then
     echo "Failed to add browser-specific settings to Firefox manifest"
     exit 1
 fi
@@ -43,7 +43,7 @@ echo "Added browser-specific settings to Firefox manifest"
 
 mv "$TMP_XPI_DIR/manifest.json.tmp" "$TMP_XPI_DIR/manifest.json"
 
-( cd "$TMP_ZIP_DIR" && zip -T -u "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.zip" * )
-( cd "$TMP_XPI_DIR" && zip -T -u "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.xpi" * )
+(cd "$TMP_ZIP_DIR" && zip -r "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.zip" *)
+(cd "$TMP_XPI_DIR" && zip -r "$BUILD_DIR_ABSOLUTE/plunger@$VERSION.xpi" *)
 
 echo "Build $VERSION complete:  [$BUILD_DIR/plunger@$VERSION.xpi, $BUILD_DIR/plunger@$VERSION.zip]"
