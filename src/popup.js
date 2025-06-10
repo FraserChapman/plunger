2d28e390f75acfd469aa3b07a87c15101a2f402c
Fraser Chapman <fraser.chapman@gmail.com>
2025-06-10 09:26:42 +0100

    Updated popup layout and functionality

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-options-btn');
+    const optionsContainer = document.getElementById('options-container');
+    const icons = {
+        true: {
+            "16": "icon16.png",
+            "32": "icon32.png",
+            "128": "icon128.png"
+        },
+        false: {
+            "16": "disabled16.png",
+            "32": "disabled32.png",
+            "128": "disabled128.png"
+        }
+    };
+
     const defaults = {
+        enabled: true,
         autoplay: false,
         privacyenhanced: false,
         cc_load_policy: false,
@@ -12,37 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         hl: 'en',
     };
 
-    document.querySelectorAll('[data-locale]').forEach(e => {
-        const message = chrome.i18n.getMessage(e.dataset.locale);
-        const lastNode = e.childNodes[e.childNodes.length - 1];
-        if (lastNode.nodeType === Node.TEXT_NODE) {
-            lastNode.textContent = message;
-        } else if (e.childNodes.length === 0) {
-            e.innerText = message;
-        }
-    });
-
-    function updateSetting(setting, value) {
-        let updateObj = {};
-        updateObj[setting] = value;
-        chrome.storage.sync.set(updateObj);
-    }
-
-    function updateIcon(enabled) {
-        const iconPaths = enabled ? {
-            "16": "icon16.png",
-            "32": "icon32.png",
-            "128": "icon128.png"
-        } : {
-            "16": "disabled16.png",
-            "32": "disabled32.png",
-            "128": "disabled128.png"
-        };
-        chrome.action.setIcon({ path: iconPaths });
-    }
-
     chrome.storage.sync.get(defaults, settings => {
-        updateIcon(settings.enabled);
         Object.keys(defaults).forEach(setting => {
             const input = document.getElementById(setting);
             if (!input) {
@@ -51,12 +37,25 @@ document.addEventListener('DOMContentLoaded', () => {
             const prop = input.type === 'checkbox' ? 'checked' : 'value';
             input[prop] = settings[setting];
             input.addEventListener('change', () => {
-                updateSetting(setting, input[prop])
+                const value = input[prop];
+                chrome?.storage?.sync.set({[setting]: value});
                 if (setting === 'enabled') {
-                    updateIcon(input[prop]);
+                    chrome?.action?.setIcon({path: icons[value]});
                 }
             });
-
         });
+    });
+
+    toggleButton.addEventListener('click', () => {
+        const isHidden = optionsContainer.hidden;
+        optionsContainer.hidden = !isHidden;
+        toggleButton.textContent = isHidden ? toggleButton.dataset.hideText : toggleButton.dataset.showText;
+    });
+
+    document.querySelectorAll('[data-locale]').forEach(elem => {
+        const message = chrome.i18n.getMessage(elem.dataset.locale);
+        if (message) {
+            elem.textContent = message;
+        }
     });
 });
